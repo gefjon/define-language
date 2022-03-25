51b6047b8c9cd0c1b9c53567f04ad074cb7cf786
Phoebe Goldman <phoebe@goldman-tribe.org>
2022-03-24 21:33:50 -0400

    allow declining a DEFINE-SINGLETON macro

--- define-language.lisp
+++ define-language.lisp
@@ -6,26 +6,28 @@
 
 (defmacro define-language ((parse-fn define-singleton define-clause)
                            &optional fallthrough-arglist &body fallthrough-transform)
-  "Define a language processor function PARSE-FUNCTION, with macros to define its behaviors DEFINE-SINGLETON \
-and DEFINE-CLAUSE.
+
+  #.(format nil "Define a language processor function PARSE-FUNCTION, with macros to define its behaviors ~
+DEFINE-SINGLETON and DEFINE-CLAUSE.
 
 PARSE-FN will be defined as a generic function of one argument.
 
 Methods of one argument can be defined by the DEFINE-SINGLETON macro, whose syntax is
   (DEFINE-SINGLETON (VAR CLOS-SPECIALIZER) &body BODY)
-CLOS-SPECIALIZER may be any specializer suitable for `defmethod' other than `cons'
+CLOS-SPECIALIZER may be any specializer suitable for `defmethod' other than `cons'.
+If DEFINE-SINGLETON is `nil', no defining macro will be generated.
 
-When PARSE-FN is invoked on an object which satisfies CLOS-SPECIALIZER, BODY will be evaluated with VAR bound \
+When PARSE-FN is invoked on an object which satisfies CLOS-SPECIALIZER, BODY will be evaluated with VAR bound ~
 to that object.
 
 Methods for lists can be defined by the DEFINE-CLAUSE macro, whose syntax is
   (DEFINE-CLAUSE HEAD ARGLIST &body BODY)
-When PARSE-FN is invoked on a list whose `first' is `eql' to HEAD, BODY will be evaluated with the ARGLIST \
+When PARSE-FN is invoked on a list whose `first' is `eql' to HEAD, BODY will be evaluated with the ARGLIST ~
 bound to the `rest' of that list as if by `destructuring-bind'.
-HEAD should usually be a symbol, but may be any object suitable for a CLOS `eql'-specializer. It will not be \
+HEAD should usually be a symbol, but may be any object suitable for a CLOS `eql'-specializer. It will not be ~
 evaluated.
 
-If a FALLTHROUGH-ARGLIST and a FALLTHROUGH-TRANSFORM are supplied, they will be used to transform lists which \
+If a FALLTHROUGH-ARGLIST and a FALLTHROUGH-TRANSFORM are supplied, they will be used to transform lists which ~
 do not match any specialized clause. FALLTHROUGH-ARGLIST may be any template suitable for `destructuring-bind'.
 
 For example, you can define a language `print-expr' with:
@@ -34,13 +36,13 @@ define its contents with:
   (define-expr (sym symbol)
     (print sym))
   (define-expr-clause foo (&rest syms)
-    (format t \"foo~{ ~a~}\" syms))
+    (format t \"foo~~{ ~~a~~}\" syms))
 and then print terms like:
   (print-expr 'foo) ; prints `FOO'
   (print-expr 'bar) ; prints `BAR'
   (print-expr '(foo foo bar) ; prints `foo FOO BAR'
   (print-expr 1) ; error
-  (print-expr '(bar)) ; error"
+  (print-expr '(bar)) ; error")
   (let* ((parse-clause (gensym (format nil "~a-CLAUSE-" parse-fn)))
          (head (gensym "HEAD-"))
          (tail (gensym "TAIL-")))
@@ -53,13 +55,14 @@ and then print terms like:
        (defgeneric ,parse-fn (term))
        (defmethod ,parse-fn ((term cons))
          (apply #',parse-clause term))
-       (defmacro ,define-singleton ((var class) &body body)
-         (when (eq class 'cons)
-           (error "Attempt to override behavior of ~a which processes ~a"
-                  ',define-singleton
-                  ',define-clause))
-         (list* 'defmethod ',parse-fn (list (list var class))
-                body))
+       ,@(when define-singleton 
+           `((defmacro ,define-singleton ((var class) &body body)
+               (when (eq class 'cons)
+                 (error "Attempt to override behavior of ~a which processes ~a"
+                        ',define-singleton
+                        ',define-clause))
+               (list* 'defmethod ',parse-fn (list (list var class))
+                      body))))
        (defmacro ,define-clause (head arglist &body body)
          (list 'defmethod ',parse-clause (list (list ',head (list 'eql (list 'quote head)))
                                                '&rest ',tail)
